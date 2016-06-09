b0bb64ed14d3e9cfb8f19f29525f0b206eceb5d9
John Preston <wcerfgba@riseup.net>
2016-06-09 20:52:47 +0100

    Commence.

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task("default", function () {
+    gulp.src("scss/**/*.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(gulp.dest("css/"));
+});
