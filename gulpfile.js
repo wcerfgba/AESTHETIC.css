403bd35d256501838ebe02773c5d8560ee46e35f
John Preston <wcerfgba@riseup.net>
2016-06-10 17:43:57 +0100

    Lots of stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task("default", function () {
-    gulp.src("scss/**/*.scss")
+    gulp.src("scss/ＡＥＳＴＨＥＴＩＣ.scss")
         .pipe(sass().on("error", sass.logError))
         .pipe(gulp.dest("css/"));
 });
